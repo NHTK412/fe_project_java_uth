298dd9d10eebdaadfca306e0363c14db68a84d1b
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-11-04 00:57:40 +0700

    chore(config): cấu hình cơ bản dự án frontend

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["dist/**"],
+  },
+
+  js.configs.recommended,
+
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    settings: { react: { version: "detect" } },
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
       },
     },
+
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // react
+      "react-refresh/only-export-components": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/display-name": "off",
+
+      // general style rules
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "no-trailing-spaces": "warn",
+      "no-multi-spaces": "warn",
+      "no-multiple-empty-lines": "warn",
+      "space-before-blocks": ["error", "always"],
+      "object-curly-spacing": ["warn", "always"],
+      indent: ["warn", 2],
+      // semi: ["warn", "never"],
+      // quotes: ["error", "single"],
+      "array-bracket-spacing": "warn",
+      "keyword-spacing": "warn",
+      // "comma-dangle": "warn",
+      "comma-spacing": "warn",
+      "arrow-spacing": "warn",
     },
   },
-])
+];
